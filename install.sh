66c6d82bb98fe4148cbc1ce5b59cdb93bea09634
Piotr Padlewski <piotr.padlewski@gmail.com>
2016-05-04 20:22:50 +0200

    added htop

--- install.sh
+++ install.sh
@@ -2,7 +2,12 @@
 sudo apt-get install \
     vim \
     git git-svn subversion gitk \
-    libedit-dev clang-3.7 clang-format-3.7 \
+    libedit-dev \
     ccache cmake-curses-gui ninja-build \
     sl \
+    htop
+
+# this might fail
+sudo apt-get install \
+    clang-3.7 clang-format-3.7 \
 
