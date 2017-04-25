c0d5ed7db37237b28770b8a85d873b1f75d34fd4
Piotr Padlewski <piotr.padlewski@gmail.com>
2017-04-25 12:27:12 +0200

    change

--- configure.sh
+++ configure.sh
@@ -5,12 +5,14 @@ mkdir ~/.vim_backup
 cp .bashrc .vimrc .gitconfig ~/
 cp yakuakerc ~/.kde/share/config/yakuakerc
 
-cd /usr/bin
 #setting gold as linker
+cd /usr/bin
 sudo ln -sf ld.gold ld
 
 cd -
 sudo cp ccache++ /usr/bin/
 # if you want to cache clang I would recomend ~40GB
 ccache -M 40GB
+
+#http://askubuntu.com/questions/72452/how-to-turn-off-compizs-drag-to-maximize-behaviour
 
